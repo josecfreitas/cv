fc0f5ad1d3682fb5b5ae6eb18387c678c4ef4c2e
José Carlos de Freitas <josecfreitas@outlook.com.br>
2021-07-04 11:13:17 -0300

    updates about me

--- src/components/cv-about.js
+++ src/components/cv-about.js
@@ -3,12 +3,13 @@ import CvSectionTitle from "./cv-section-title"
 
 const CvAbout = () => (
 	<>
-		<CvSectionTitle>About</CvSectionTitle>
+		<CvSectionTitle>About Me</CvSectionTitle>
 		<p>
-			Hi. My name is José Carlos de Freitas; I am a 25-year-old boy from Brazil, and I have been studying
-			programming since I was 15. I have associate’s, bachelor’s and master’s degree in computer science with 2
-			articles published in international events. In my career, I have already done 190+ courses at Alura (tech
-			courses platform), and developed 30+ end to end projects, from Landing Pages to Apps and Systems.
+			Hi, 😉 My name is José Carlos de Freitas; I am a boy from Brazil and I have been studying
+			programming since I was 15 (2011). I have associate’s, bachelor’s, and master’s degrees
+			in computer science with 2 articles published in international events. In my career, I
+			have already done 190+ courses at Alura (tech courses platform), and developed 30+ end-to-end
+			projects, from Landing Pages to Apps and Systems.
 		</p>
 		<p>
 			I am passionate about Computer Science and learning, interested in DevOps, and I love to solve problems with
